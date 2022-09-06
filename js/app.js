ecb2447099a3805691a994d98bcd7b8a43733e99
Diego <diegoj9999@gmail.com>
2022-09-06 16:49:56 -0300

    Mi primer commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav__menu");
+
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("nav__menu--visible");
+
+});
